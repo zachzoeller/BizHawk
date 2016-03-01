a287b33074baf9fddfbe74eefe726ece0ed46113
Zach Zoeller <Zach Zoeller>
2016-03-01 15:44:32 -0600

    Added files per this article: http://stackoverflow.com/questions/33428382/add-luasocket-to-program-bizhawk-shipped-with-own-lua-environment

---
+++ output/Lua/testLua.lua
@@ -0,0 +1,5 @@
+-- print(require("socket.http").request{ url = "http://www.google.com" });
+print(require("socket.http").request{ url = "http://jsonplaceholder.typicode.com/posts/1" });
+
+
+--http://jsonplaceholder.typicode.com/posts/1
---
+++ output64/testingLua.lua
@@ -0,0 +1,22 @@
+require("socket.http").request{ url = "http://localhost:34039/api/pool" };
+-- requests = require('requests')
+-- response = requests.get('http://httpbin.org/get')
+-- print(response.status_code)
+
+--print(http.request("http://localhost:34039/api/Pool"))
+
+--localhost:34039/api/pool/
+--print(http.request("http://www.google.com"))
+
+-- req = http.request{url="http://localhost:34039/api/pool"};
+-- print(req);
+-- function stuff()
+-- 	http = require("socket.http")
+-- 	req = http.request{ url = "http://localhost:34039" }
+-- 	print(req);
+-- end
+
+-- if pcall(stuff) then
+-- 	print('worked')
+-- else
+-- 	print(error)
